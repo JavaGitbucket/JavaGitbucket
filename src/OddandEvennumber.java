6c5e13c67aec558dec671d20dc57733c28d17014
JavaGitbucket <urviarjunpatel@gmail.com>
2022-08-31 15:36:46 -0400

    Hello commit.............

---
+++ src/OddandEvennumber.java
@@ -0,0 +1,13 @@
+
+public class OddandEvennumber {
+
+	public static void main(String[] args) {
+		int a=5;
+		if (a==5) {System.out.println("a is a odd Number.");
+		}
+		else {System.out.println("a is a even Number.");
+		}	
+
+	}
+
+}
