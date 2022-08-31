ee8e2e4de7bd11ebdaf1bede18fab46012f9d27f
JavaGitbucket <urviarjunpatel@gmail.com>
2022-08-31 15:56:44 -0400

    add line 6 & 7

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	
 
-	
-		System.out.println("print a");
+		System.out.println("print 1");
+		System.out.println("print 2");
 
 	}
 
