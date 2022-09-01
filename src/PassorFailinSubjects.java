dfe37550891eec4d1dedcad8932648cf4db99f48
JavaGitbucket <urviarjunpatel@gmail.com>
2022-09-01 09:36:06 -0400

    third commit....

--- src/PassorFailinSubjects.java
+++ src/PassorFailinSubjects.java
@@ -1,17 +1,26 @@
-/**
- * 
- */
 
-/**
- * @author urvip
- *
- */
 public class PassorFailinSubjects {
+	
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
+		int English,Science,Maths;
+		float total,percentage,average;
+		
+		System.out.println("please enter three subject marks:");
+		English=60;
+		Science=50;
+		Maths=90;
+		int Total = English+Science+Maths;
+		Object  Average = Total/3;
+		int Percentage = (Total*300)/100;
+		System.out.println("Total  marks =" +Total);
+		System.out.println("Average  marks =" +Average);
+		System.out.println("Percentage  marks =" +Percentage);
+		
+		
+		
+		
 	
 
 	}
